295e562f9946de2fab3ededb92ea2319dea85f22
Heath Lester <h.allen.lester@gmail.com>
2021-02-17 16:37:26 -0600

    added game framework

---
+++ src/components/game/GameDetails.js
@@ -0,0 +1,22 @@
+import React, { useContext, useEffect } from "react"
+import { GameContext } from "./GameProvider.js"
+
+export const GameList = (props) => {
+    const { games, getGames } = useContext(GameContext)
+
+    useEffect(() => {
+        getGames()
+    }, [])
+
+    return (
+        <>
+            <article className="games">
+                <section key={`game--${game.id}`} className="game">
+                    <div className="game__title">{game.title} by {game.maker}</div>
+                    <div className="game__players">{game.number_of_players} players needed</div>
+                    <div className="game__skillLevel">Skill level is {game.skill_level}</div>
+                </section>
+            </article>
+        </>
+    )
+}
